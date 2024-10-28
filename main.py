8b53650ad22a0934b303ce002504a9023f91f953
andrewsmallfield <asmallfield@unomaha.edu>
2024-10-28 16:24:33 +0000

    done

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ def main():
   team_data = []
 
   #get the import info out of pitching and store into team_data
-
+  for team in pitching:
+    data = team.split(",")
+    team_data.append([data[0], "", data[3], data[4], data[5], data[7]])
+    #team, blank, RA/G, W, L, ERA
   pitching.close()
 
   hitting = open("MLB_Hitting.csv", 'r')
-
+  lineNum = 0
+  for team in hitting:
+    data = team.split(",")
+    team_data[lineNum][1]= data[3]
+    lineNum = lineNum + 1
   #process all the data in hitting and add to the correct portion of the team_data
-
   hitting.close()
 
 
@@ -17,6 +23,14 @@ def main():
 
   #process each line of the team_data and save to the output file.
   output = ""
+  for team in team_data:
+    team = str(team)
+    team = team.replace("[", "")
+    team = team. replace("]", "")
+    team = team.replace("'", "")
+    print(team)
+    output = output + team +"\n"
+
   outFile.write(output)
 
   outFile.close()
